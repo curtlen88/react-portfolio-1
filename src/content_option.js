3d7c6382b5f6a9fecc93e895d677d6176b62cdf4
curtlen88 <curtlen88@gmail.com>
2023-03-03 10:57:43 -0700

    project links descriptions

--- src/content_option.js
+++ src/content_option.js
@@ -11,7 +11,7 @@ const introdata = {
         second: "Sense then, I have graduated from General Assembly's Software Engineering Immersive Bootcamp",
         third: "Now I am hooked on coding...",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Software Engineer, new father, dog and nature lover, with a background in the construction industry. I am a hard worker, a fast learner, and a team player. I am looking for a position where I can use my skills to help a company grow and succeed.",
     your_img_url: "https://live.staticflickr.com/65535/52719281578_6ea7958e1e_b.jpg",
 };
 
@@ -20,19 +20,29 @@ const dataabout = {
     aboutme: "As a Software Engineer with a background in construction, I bring a unique set of skills to my work. My coachable attitude, collaborative work experience, attention to detail, and strong time management skills are essential in both industries. I apply these qualities to deliver high-quality code, drawing on my experience in construction to approach coding with precision and care. For example, my attention to detail helps me catch bugs and errors, resulting in more reliable and stable software. Additionally, my experience in construction has taught me the importance of planning and collaboration, which I apply in my work to ensure timely delivery of projects that meet or exceed customer expectations.",
 };
 const worktimeline = [{
-        jobtitle: "General Assembly SEI Graduate",
+        jobtitle: "General Assembly SEI - Graduate",
         where: "Online",
         date: "Nov 2022 - Feb 2023",
     },
     {
-        jobtitle: "Carpenter",
-        where: "Arvada",
+        jobtitle: "Richmond Homes - Superintendant",
+        where: "North Denver, CO",
+        date: "Nov 2021 - Nov 2022",
+    },
+    {
+        jobtitle: "Self Employed - Carpenter",
+        where: "Arvada, CO",
         date: "Aug 2019 - Present",
     },
     {
-        jobtitle: "Superintendant",
-        where: "North Denver",
-        date: "Nov 2021 - Nov 2022",
+        jobtitle: "Div6 Millwork - Assistant Project Manager",
+        where: "Boulder, CO",
+        date: "Oct 2018 - Jul 2019",
+    },
+    {
+        jobtitle: "Absolute Carpentry - Foreman",
+        where: "Thornton, CO",
+        date: "Oct 2017 - Oct 2018",
     },
 ];
 
@@ -58,46 +68,41 @@ const skills = [{
     },
 ];
 
-const services = [{
-        title: "Full Stack Development",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-];
+// const services = [{
+//         title: "Full Stack Development",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Mobile Apps",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Wordpress Design",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+// ];
 
 const dataportfolio = [{
         img:"https://live.staticflickr.com/65535/52719281578_6ea7958e1e_b.jpg",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        desctiption: "My first ever project, a simple obstacle avoidance game of X-Wings vs Tie Fighters.",
+        link: "https://curtlen88.github.io/XWings-vs-TieFighters/",
     },
     {
         img:"https://live.staticflickr.com/65535/52718770421_90b9e9979e_h.jpg",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        desctiption: "Second project with GA, a full stack Cocktail Recipe app. Login or create and account to save your favorite recipes.",
+        link: "https://drinktales-app.herokuapp.com/",
     },
     {
         img: "https://live.staticflickr.com/65535/52719261158_88ed74def2_h.jpg",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        desctiption: "Third project with GA, a full stack app that allows users to create and share their favorite sustainable living habits.",
+        link: "https://ubiquitous-seahorse-b5185a.netlify.app",
     }
 ];
 
 const contactConfig = {
     YOUR_EMAIL: "curtlen88@gmail.com",
     YOUR_FONE: "(303)549-0612",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    description: "I am excited about the opportunity to continue learning and growing as a Software Engineer and I am committed to building high-quality software that makes a difference in the world. Thank you for reviewing my portfolio, I look forward to hearing from you!",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -116,7 +121,7 @@ export {
     dataportfolio,
     worktimeline,
     skills,
-    services,
+    // services,
     introdata,
     contactConfig,
     socialprofils,
